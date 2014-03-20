6a5c509177f76213b007b5a3d9f0e9e2f17c0c59
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-20 15:15:38 +0100

    Added list of input/output formats.

--- src/edeposit/amqp/calibre/__init__.py
+++ src/edeposit/amqp/calibre/__init__.py
@@ -8,16 +8,67 @@ from collections import namedtuple
 
 import sh
 
+
 #= Variables ==================================================================
 
 
 
 #= Functions & objects ========================================================
+INPUT_FORMATS = [
+    "cbz",
+    "cbr",
+    "cbc",
+    "chm",
+    "djvu",
+    "docx",
+    "epub",
+    "fb2",
+    "html",
+    "htmlz",
+    "lit",
+    "lrf",
+    "mobi",
+    "odt",
+    "pdf",
+    "prc",
+    "pdb",
+    "pml",
+    "rb",
+    "rtf",
+    "snb",
+    "tcr",
+    "txt",
+    "txtz"
+]
+
+OUTPUT_FORMATS = [
+    "azw3",
+    "epub",
+    "fb2",
+    "oeb",
+    "lit",
+    "lrf",
+    "mobi",
+    "htmlz",
+    "pdb",
+    "pml",
+    "rb",
+    "pdf",
+    "rtf",
+    "snb",
+    "tcr",
+    "txt",
+    "txtz"
+]
+
+
 class ConversionRequest(namedtuple("ConversionRequest", [])):
     pass
 
+
 class ConversionResponse(namedtuple("ConversionResponse", [])):
     pass
+
 
 
 def reactToAMQPMessage(message, response_callback, UUID):
