3b5285c3e3d71da947d3765549d8ae6f9fb4d985
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-26 12:30:24 +0100

    Added check for ebook-convert.

--- src/edeposit/amqp/calibre/calibre.py
+++ src/edeposit/amqp/calibre/calibre.py
@@ -4,7 +4,7 @@
 # Interpreter version: python 2.7
 #
 #= Imports ====================================================================
-
+import sh
 
 
 #= Variables ==================================================================
@@ -12,9 +12,31 @@
 
 
 #= Functions & objects ========================================================
+def check():
+    """
+    Check, if the 'ebook-convert' program is installed.
 
+    Raises:
+        UserWarning: if not.
+    """
+    try:
+        output = sh.ebook_convert("ebook-convert", _ok_code=[1])
+    except sh.CommandNotFound:
+        raise UserWarning(
+            "'ebook-convert' not found. Do you have callibre installed?"
+        )
+
+    if "Usage:" not in output:
+        raise UserWarning(
+            "'ebook-convert' reacts strangely. Post this to developers:\n" +
+            output
+        )
+
+
+def convert(input_type, output_type, b64_data):
+    pass
 
 
 #= Main program ===============================================================
 if __name__ == '__main__':
-    pass
+    check()
